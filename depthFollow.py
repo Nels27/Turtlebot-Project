6bc4d1f5257b7411af8a85a9a018c471379e8aaa
fmyousuf3 <fyousuf3@gatech.edu>
2017-12-04 15:20:52 -0500

    Added depth information

--- depthFollow.py
+++ depthFollow.py
@@ -13,25 +13,30 @@ class Follower:
 
                 self.image_sub = rospy.Subscriber('camera/depth/image_raw',Image, self.image_callback)
 
+
                 self.cmd_vel_pub = rospy.Publisher('cmd_vel_mux/input/teleop',Twist, queue_size=1)
 
                 self.twist = Twist()
 
-        def image_callback(self, msg):
+        def image_callback(self, msg, ros_image):
 
                 image = self.bridge.imgmsg_to_cv2(msg,desired_encoding='bgr8')
+                depth_image = self.bridge.imgmsg_to_cv2(ros_image, "passthrough")
                 hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
                 lower_thresh = numpy.array([0, 100, 100])
                 upper_thresh = numpy.array([20, 255, 255])
                 mask = cv2.inRange(hsv, lower_thresh, upper_thresh)
 
-                h, w, d = image.shape
+                h, w, d = depth_image.shape
+
+                depth_array = numpy.array(depth_image, dtype=numpy.float32)
+                cv2.normalize(depth_array, depth_array, 0, 1, cv2.NORM_MINMAX)
 
                 M = cv2.moments(mask)
                 if M['m00'] > 0:
                         cx = int(M['m10']/M['m00'])
                         cy = int(M['m01']/M['m00'])
-                        cv2.circle(image, (cx, cy), 20, (0,0,255), -1)
+                        cv2.circle(depth_array, (cx, cy), 20, (0,0,255), -1)
                         err = cx - w/2
                         foward_err = cy - h/2
                         limitaion = int(M['m00'])
